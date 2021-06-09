ca420691495cc1f0aa3c43b657de5c002aee15eb
bdrkreza <diureza@gamil.com>
2021-06-09 16:09:36 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from "./Components/Header/Navbar/Navbar";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/Components/Header/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from '../../HeroSection/HeroSection';
+
+const Header = () => {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    );
+};
+
+export default Header;
